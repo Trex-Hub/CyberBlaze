b960b8595e8954179d909d3dc9259ef88fb9dec3
sanjus2502 <sanjus2502@gmail.com>
2024-03-20 14:53:38 +0530

    Input Sanitization, Validation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,130 +3,143 @@ import { Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import RowAndColumnSpacing from './ProfileContent';
 import Button from '@mui/material/Button';
-import CustomTextField from './CustomTextField'
+import CustomTextField from './CustomTextField';
 import FAQ from './FAQ';
 import ReportHere from './ReportHere';
-
+import {sanitizeInput} from './utils'
 
 const customFont = {
-    fontFamily: '"Lexend Deca", sans-serif',
-    fontOpticalSizing: 'auto',
-    fontStyle: 'normal'
+  fontFamily: '"Lexend Deca", sans-serif',
+  fontOpticalSizing: 'auto',
+  fontStyle: 'normal'
 };
 
 const instaColor = {
-    background: 'linear-gradient(to right, #feda75, #fa7e1e, #d62976, #962fbf, #4f5bd5)',
-    color: 'white',
-    padding: '10px 20px',
-    border: 'none',
-    borderRadius: '5px',
-    cursor: 'pointer'
+  background: 'linear-gradient(to right, #feda75, #fa7e1e, #d62976, #962fbf, #4f5bd5)',
+  color: 'white',
+  padding: '10px 20px',
+  border: 'none',
+  borderRadius: '5px',
+  cursor: 'pointer'
 };
+
 const gradientStyle = {
-    background: 'linear-gradient(to right, #feda75, #fa7e1e, #d62976, #962fbf, #4f5bd5)',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-
+  background: 'linear-gradient(to right, #feda75, #fa7e1e, #d62976, #962fbf, #4f5bd5)',
+  WebkitBackgroundClip: 'text',
+  WebkitTextFillColor: 'transparent',
 };
-
 
 class Hero extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            username: '',
-            searchClicked: false,
-        };
-    }
-
-    handleUsernameChange = (event) => {
-        this.setState({ username: event.target.value });
+    this.state = {
+      username: '',
+      searchClicked: false,
+      usernameError: ''
     };
+  }
 
-    handleSearchClick = () => {
-        this.setState({ searchClicked: true });
-    };
-
-    render() {
-        const inputStyles = {
-            input: {
-                color: 'whitesmoke', // Text color
-                '&::placeholder': {
-                    color: 'white', // Placeholder color
-                },
-                '&:-webkit-input-placeholder': {
-                    color: 'white', // Placeholder color for Safari/Chrome
-                },
-                '&::-moz-placeholder': {
-                    color: 'white', // Placeholder color for Firefox
-                    opacity: 1, // Ensure compatibility with Firefox
-                },
-                '&:-ms-input-placeholder': {
-                    color: 'white', // Placeholder color for IE
-                },
-                '&:focus': {
-                    borderColor: 'white !important', // Border color on focus
-                },
-            },
-        };
-        return (
-            <div  style={{ backgroundColor: ' ' }}>
-                <div style={{ textAlign: 'center' }}>
-
-
-                    <Typography variant="h3" gutterBottom style={{ ...customFont, fontWeight: 600, marginBottom: '0.3rem', marginTop: '12rem', color: 'whitesmoke' }}>
-                        Social Media Profile
-                    </Typography>
-                    <Typography variant="h3" gutterBottom style={{ ...customFont, fontWeight: 600, marginBottom: '1rem', marginTop: '0.3rem', color: 'whitesmoke' }}>
-                        <span style={gradientStyle}>
-                            Authentication {' '}
-                        </span>
-                        Portal
-                    </Typography>
-                    <Typography variant="h5" gutterBottom style={{ ...customFont, fontWeight: 500, marginBottom: '12rem', color: 'whitesmoke' }}>
-                        Let's Secure your online identity
-                    </Typography>
-
-                    <Box display="flex" justifyContent="center" marginBottom={17} marginTop={10}>
-                        <CustomTextField
-                            label="Enter Username"
-                            id="username"
-                            style={{ ...inputStyles, width: '40%', marginRight: '10px' }}
-                            value={this.state.username}
-                            onChange={this.handleUsernameChange}
-                            InputLabelProps={{
-                                style: { color: 'whitesmoke' } // Placeholder color
-                            }}
-                            InputProps={{
-                                style: inputStyles.input,
-                            }}
-                        />
-
-
-
-                        <Button variant="contained" onClick={this.handleSearchClick} style={{ ...instaColor, width: '12vw' }}>Search</Button>
-
-                    </Box>
-                </div>
-
-                <Box display="flex" justifyContent="center" >
-                    <RowAndColumnSpacing username={this.state.username} searchClicked={this.state.searchClicked} />
-                </Box>
-
-                <div id="Report" style={{ backgroundColor: 'white' }}>
-                    <ReportHere />
-                </div>
-
-                <div style={{ backgroundColor: 'white' }}>
-
-                    <FAQ >
-
-                    </FAQ>
-                </div>
-            </div>
-        );
+  validateUsername = () => {
+    const { username } = this.state;
+    if (!username.trim()) {
+      this.setState({ usernameError: 'Username cannot be empty' });
+      return false;
     }
+    if (username.length > 30) {
+      this.setState({ usernameError: 'Username cannot be longer than 30 characters' });
+      return false;
+    }
+    // Additional validation to prevent XSS and SQL Injection
+    if (!sanitizeInput(username)) {
+      this.setState({ usernameError: 'Invalid characters in the username' });
+      return false;
+    }
+    return true;
+  };
+
+  handleUsernameChange = (event) => {
+    this.setState({ username: event.target.value, usernameError: '' });
+  };
+
+  handleSearchClick = () => {
+    if (this.validateUsername()) {
+      this.setState({ searchClicked: true });
+    }   
+  };
+
+  render() {
+    const { username, usernameError, searchClicked } = this.state;
+    const inputStyles = {
+      input: {
+        color: 'whitesmoke',
+        '&::placeholder': {
+          color: 'white',
+        },
+        '&:-webkit-input-placeholder': {
+          color: 'white',
+        },
+        '&::-moz-placeholder': {
+          color: 'white',
+          opacity: 1,
+        },
+        '&:-ms-input-placeholder': {
+          color: 'white',
+        },
+        '&:focus': {
+          borderColor: 'white !important',
+        },
+      },
+    };
+    return (
+      <div style={{ backgroundColor: '' }}>
+        <div style={{ textAlign: 'center' }}>
+          <Typography variant="h3" gutterBottom style={{ ...customFont, fontWeight: 600, marginBottom: '0.3rem', marginTop: '12rem', color: 'whitesmoke' }}>
+            Social Media Profile
+          </Typography>
+          <Typography variant="h3" gutterBottom style={{ ...customFont, fontWeight: 600, marginBottom: '1rem', marginTop: '0.3rem', color: 'whitesmoke' }}>
+            <span style={gradientStyle}>
+              Authentication {' '}
+            </span>
+            Portal
+          </Typography>
+          <Typography variant="h5" gutterBottom style={{ ...customFont, fontWeight: 500, marginBottom: '12rem', color: 'whitesmoke' }}>
+            Let's Secure your online identity
+          </Typography>
+          <Box noValidate display="flex" justifyContent="center" marginBottom={17} marginTop={10}>
+            <CustomTextField
+              label="Enter Username"
+              id="username"
+              style={{ ...inputStyles, width: '40%', marginRight: '10px' }}
+              value={username}
+              error={!!usernameError}
+              helperText={usernameError}
+              onChange={this.handleUsernameChange}
+              InputLabelProps={{
+                style: { color: 'whitesmoke' }
+              }}
+              InputProps={{
+                style: inputStyles.input,
+              }}
+            />
+            <form action="" method='POST'>
+
+            <Button variant="contained" onClick={this.handleSearchClick} style={{ ...instaColor, width: '12vw', lineHeight: '2.5' }}>Search</Button>
+            </form>
+          </Box>
+        </div>
+        <Box display="flex" justifyContent="center">
+          <RowAndColumnSpacing username={username} searchClicked={searchClicked} />
+        </Box>
+        <div id="Report" style={{ backgroundColor: 'white' }}>
+          <ReportHere />
+        </div>
+        <div style={{ backgroundColor: 'white' }}>
+          <FAQ />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Hero;
--- src/components/ProfileContent.jsx
+++ src/components/ProfileContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
